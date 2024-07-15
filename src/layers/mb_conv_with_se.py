5e02da229cf6dab0239b49e8fb47a7c92edbc3eb
RyanWri <ranman37@gmail.com>
2024-07-15 15:25:53 +0300

    adjusting shapes and fixing errors

--- src/layers/mb_conv_with_se.py
+++ src/layers/mb_conv_with_se.py
@@ -1,6 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
 class SE(nn.Module):
@@ -15,7 +21,8 @@ class SE(nn.Module):
         )
 
     def forward(self, x):
-        return x * self.se(x)
+        scale = self.se(x)
+        return x * scale
 
 
 class MBConv(nn.Module):
@@ -32,18 +39,12 @@ class MBConv(nn.Module):
         super(MBConv, self).__init__()
         hidden_dim = int(expand_rate * in_dim)
 
-        # 1x1 Conv (Expand Convolution):
-        # Expands the number of channels using a 1x1 convolution.
-        # Followed by Batch Normalization and GELU activation.
         self.expand_conv = nn.Sequential(
             nn.Conv2d(in_dim, hidden_dim, 1, bias=False),
             nn.BatchNorm2d(hidden_dim),
             nn.GELU(),
         )
 
-        # 3x3 Depthwise Conv:
-        # Performs depthwise convolution with a 3x3 kernel, maintaining spatial dimensions and reducing computational complexity.
-        # Followed by Batch Normalization and GELU activation.
         self.dw_conv = nn.Sequential(
             nn.Conv2d(
                 hidden_dim,
@@ -58,36 +59,31 @@ class MBConv(nn.Module):
             nn.GELU(),
         )
 
-        # Squeeze-and-Excitation Block:
-        # Applies a global average pooling to generate channel-wise statistics.
-        # Followed by two pointwise convolutions (1x1) and GELU activation to model channel-wise dependencies.
-        # Finally, applies a sigmoid activation to scale the input.
         self.se = SE(hidden_dim, max(1, int(in_dim * se_rate)))
 
-        # 1x1 Conv (Output Convolution):
-        # Uses a 1x1 convolution to project the expanded channels back to the output dimension.
-        # Followed by Batch Normalization.
         self.out_conv = nn.Sequential(
             nn.Conv2d(hidden_dim, out_dim, 1, bias=False), nn.BatchNorm2d(out_dim)
         )
 
-        # Residual Connection:
-        # Ensures the input is directly added to the output.
-        # If the input and output dimensions do not match, a 1x1 convolution with Batch Normalization is applied to the input to match dimensions.
         self.proj = (
-            nn.Sequential(
+            nn.Identity()
+            if in_dim == out_dim and stride_size == 1
+            else nn.Sequential(
                 nn.Conv2d(in_dim, out_dim, 1, bias=False), nn.BatchNorm2d(out_dim)
             )
-            if in_dim != out_dim or stride_size > 1
-            else nn.Identity()
         )
 
     def forward(self, x):
         residual = x
+        logger.info(f"MBConv input shape: {x.shape}")
         x = self.expand_conv(x)
         x = self.dw_conv(x)
+        logger.info(f"MBConv Expand and depthwise convolution completed")
         x = self.se(x)
+        logger.info(f"MBConv squeeze and excitation completed")
         x = self.out_conv(x)
-        if self.proj is not nn.Identity:
+        if not isinstance(self.proj, nn.Identity):
             residual = self.proj(residual)
-        return x + residual
+        mb_conv_se_output = x + residual
+        logger.info(f"MBConv with SE output shape: {mb_conv_se_output.shape}")
+        return mb_conv_se_output
